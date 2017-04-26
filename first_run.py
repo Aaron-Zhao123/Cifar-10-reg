dede7f39a36826bf333d7f9640bc79dac99a74c2
Aaron-Zhao123 <aaronzhao0731@gmail.com>
2017-04-26 10:04:13 +0100

    first commit

--- first_run.py
+++ first_run.py
@@ -21,7 +21,7 @@ retrain = 0
 f_name = compute_file_name(pcov, pfc)
 parent_dir = './'
 # lr = 1e-5
-lr = 1e-4
+lr = 1e-3
 with_biases = False
 param = [
     ('-pcov1',pcov[0]),
@@ -29,15 +29,15 @@ param = [
     ('-pfc1',pfc[0]),
     ('-pfc2',pfc[1]),
     ('-pfc3',pfc[2]),
-    ('-first_time', True),
+    ('-first_time', False),
     ('-file_name', f_name),
     ('-train', True),
     ('-prune', False),
     ('-lr', lr),
     ('-with_biases', with_biases),
     ('-parent_dir', parent_dir),
-    ('-lambda1', 1e-3),
-    ('-lambda2', 1e-3)
+    ('-lambda1', 1e-4),
+    ('-lambda2', 1e-5)
     ]
 
 _ = train.main(param)
