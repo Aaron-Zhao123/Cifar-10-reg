18ba4e13797587cf433ddb3615e90633112e3639
Aaron-Zhao123 <aaronzhao0731@gmail.com>
2017-04-28 19:52:01 +0100

    retrain

--- run.py
+++ run.py
@@ -16,30 +16,14 @@ count = 0
 # pcov = [10., 66.]
 # pfc = [85., 66., 10.]
 pcov = [0., 0.]
-pfc = [60., 0., 0.]
+pfc = [0., 0., 0.]
 retrain = 0
 f_name = compute_file_name(pcov, pfc)
-parent_dir = 'assets/withbiases/'
+parent_dir = 'assets/reg_pruning/'
 # lr = 1e-5
 lr = 1e-4
 
-# f_name = 'pruningv00'
 # initial run
-param = [
-    ('-pcov1',pcov[0]),
-    ('-pcov2',pcov[1]),
-    ('-pfc1',pfc[0]),
-    ('-pfc2',pfc[1]),
-    ('-pfc3',pfc[2]),
-    ('-first_time', False),
-    ('-file_name', f_name),
-    ('-train', True),
-    ('-prune', False),
-    ('-lr', lr),
-    ('-with_biases', True),
-    ('-parent_dir', parent_dir)
-    ]
-# acc = train.main(param)
 param = [
     ('-pcov1',pcov[0]),
     ('-pcov2',pcov[1]),
@@ -52,7 +36,9 @@ param = [
     ('-prune', False),
     ('-lr', lr),
     ('-with_biases', True),
-    ('-parent_dir', parent_dir)
+    ('-parent_dir', parent_dir),
+    ('-lambda1', 1e-4),
+    ('-lambda2', 1e-5)
     ]
 test_acc = train.main(param)
 print("first train")
@@ -60,20 +46,9 @@ acc_list.append((pcov[:],pfc[:],test_acc))
 print('accuracy summary: {}'.format(acc_list))
 
 run = 1
-
-level1 = 1
-level2 = 0
-level3 = 0
-level4 = 0
-level5 = 0
-level6 = 0
-
-working_level = level1
 hist = [(pcov, pfc, test_acc)]
-# pcov = [20., 66.]
-# pfc = [85., 66., 20.]
 pcov = [0., 0.]
-pfc = [60., 0., 0.]
+pfc = [0., 0., 0.]
 retrain_cnt = 0
 roundrobin = 0
 with_biases = True
@@ -91,7 +66,9 @@ while (run):
         ('-prune', True),
         ('-lr', lr),
         ('-with_biases', with_biases),
-        ('-parent_dir', parent_dir)
+        ('-parent_dir', parent_dir),
+        ('-lambda1', 1e-4),
+        ('-lambda2', 1e-5)
         ]
     _ = train.main(param)
 
@@ -111,7 +88,9 @@ while (run):
         ('-prune', False),
         ('-lr', lr),
         ('-with_biases', with_biases),
-        ('-parent_dir', parent_dir)
+        ('-parent_dir', parent_dir),
+        ('-lambda1', 1e-4),
+        ('-lambda2', 1e-5)
         ]
     _ = train.main(param)
 
@@ -129,68 +108,27 @@ while (run):
         ('-prune', False),
         ('-lr', lr),
         ('-with_biases', with_biases),
-        ('-parent_dir', parent_dir)
+        ('-parent_dir', parent_dir),
+        ('-lambda1', 1e-4),
+        ('-lambda2', 1e-5)
         ]
     acc = train.main(param)
     hist.append((pcov, pfc, acc))
     f_name = compute_file_name(pcov, pfc)
     # pcov[1] = pcov[1] + 10.
-    if (acc > 0.823):
+    if (acc > 0.808):
         pfc[0] = pfc[0] + 5.
-        # pfc[0] = pfc[0] + 1.
-        # pfc[1] = pfc[1] + 10.
-        # pcov[1] = pcov[1] + 10.
-        # pfc[2] = pfc[2] + 10.
-        # pcov[0] = pcov[0] + 10.
         lr = 1e-4
         retrain = 0
-        roundrobin = 0
         acc_list.append((pcov,pfc,acc))
     else:
         retrain = retrain + 1
         if (retrain == 5):
-            lr = lr / float(10)
+            lr = lr / float(2)
         if (retrain == 10):
-            lr = lr / float(10)
+            lr = lr / float(5)
         if (retrain > 15):
             break
-            #     if (level3 == 1):
-            #         level3 = 0
-            #         level1 = 1
-            # if (roundrobin > 2):
-    # pcov[1] = pcov[1] + 10.
-    # if (pfc[0] > 90.):
-    #     run = 0
-    # if (working_level == level1):
-    #     if (acc >= 0.8):
-    #         f_name = compute_file_name(pcov, pfc)
-    #         pfc[0] = pfc[0] + 10.
-    #     else:
-    #         pfc[0] = pfc[0] - 10.
-    #         f_name = compute_file_name(pcov, pfc)
-    #         working_level = level2
-    #         run = 0
-    # if (working_level == level2):
-    #     if (acc >= 0.8):
-    #         f_name = compute_file_name(pcov, pfc)
-    #         pcov = []pcov + [1., 1.]
-    #         pfc =  pfc +[1., 1., 1.]
-    #     else:
-    #         pcov = pcov - [1., 1.]
-    #         pfc =  pfc -[1., 1., 1.]
-    #         f_name = compute_file_name(pcov, pfc)
-    #         pcov = pcov + [0.1, 0.1]
-    #         pfc =  pfc +[0.1, 0.1, 0.1]
-    #         working_level = level3
-    # if (working_level == level3):
-    #     if (acc >= 0.8):
-    #         f_name = compute_file_name(pcov, pfc)
-    #         pcov = pcov + [0.1, 0.1]
-    #         pfc =  pfc +[0.1, 0.1, 0.1]
-    #     else:
-    #         run = 0
-    #         print('finished')
-    #
     count = count + 1
     print('accuracy summary: {}'.format(acc_list))
     print (acc)
