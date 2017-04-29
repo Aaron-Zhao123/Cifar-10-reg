1b25af454367b83d21cf11a3cc19e47c1a49efcf
Aaron-Zhao123 <aaronzhao0731@gmail.com>
2017-04-29 16:14:34 +0100

    train with crates

--- train.py
+++ train.py
@@ -104,9 +104,9 @@ def prune_weights(cRates, weights, weights_mask, biases, biases_mask, mask_dir, 
         mask_on = np.abs(w_eval) > threshold_on
         new_mask[key] = np.logical_or(((1 - mask_off) * weights_mask[key]),mask_on).astype(int)
     with open(mask_dir + f_name, 'wb') as f:
-        pickle.dump((weights_mask,biases_mask), f)
+        pickle.dump((new_mask,biases_mask), f)
     print(mask_dir + f_name)
-    prune_info(weights_mask, 1)
+    prune_info(new_mask, 1)
     sys.exit()
 
 def initialize_weights_mask(first_time_training, mask_dir, file_name):
