d6f913ab7ff060f836fb0f3307a1ba6abe5d7762
Aaron-Zhao123 <aaronzhao0731@gmail.com>
2017-05-01 00:37:23 +0100

    changed test

--- run_for_testacc.py
+++ run_for_testacc.py
@@ -13,16 +13,22 @@ def compute_file_name(pcov, pfc):
 
 acc_list = []
 count = 0
-pcov = [0., 0.]
-pfc = [0., 0., 0.]
+crates = {
+    'cov1': 0.,
+    'cov2': 0.,
+    'fc1': 3.5,
+    'fc2': 0.,
+    'fc3': 0.
+}
 retrain = 0
 f_name = compute_file_name(pcov, pfc)
 
 # f_name = 'pruningv00'
 # initial run
 parent_dir = '/Users/aaron/Projects/Mphil_project/tmp_cifar10/async_pruning/'
-parent_dir = 'assets/withbiases/'
+parent_dir = 'assets'
 parent_dir = './'
+with_biases = False
 
 run = 1
 hist = []
@@ -34,67 +40,21 @@ lr = 1e-5
 # Prune
 while (run):
 
-    f_name = compute_file_name(pcov, pfc)
-
     # TEST
     param = [
-        ('-pcov1',pcov[0]),
-        ('-pcov2',pcov[1]),
-        ('-pfc1',pfc[0]),
-        ('-pfc2',pfc[1]),
-        ('-pfc3',pfc[2]),
+        ('-cRates', crates),
         ('-first_time', False),
-        ('-file_name', f_name),
         ('-train', False),
         ('-prune', False),
-        ('-parent_dir', parent_dir),
         ('-lr', lr),
+        ('-with_biases', with_biases),
         ('-parent_dir', parent_dir),
-        ('-lambda1', 1e-3),
-        ('-lambda2', 1e-3)
+        ('-lambda1', 1e-4),
+        ('-lambda2', 1e-5)
         ]
     acc = train.main(param)
-    hist.append((pcov[:], pfc[:], acc))
-    print(pfc)
-    print(hist)
-    f_name = compute_file_name(pcov, pfc)
-    # pcov[1] = pcov[1] + 10.
-    # pfc[0] = pfc[0] + 10.
-    # if (pfc[0] > 90):
-        # run = 0
+    print(acc)
     run = 0
-    # if (working_level == level1):
-    #     if (acc >= 0.8):
-    #         f_name = compute_file_name(pcov, pfc)
-    #         pfc[0] = pfc[0] + 10.
-    #     else:
-    #         pfc[0] = pfc[0] - 10.
-    #         f_name = compute_file_name(pcov, pfc)
-    #         working_level = level2
-    #         run = 0
-    # if (working_level == level2):
-    #     if (acc >= 0.8):
-    #         f_name = compute_file_name(pcov, pfc)
-    #         pcov = []pcov + [1., 1.]
-    #         pfc =  pfc +[1., 1., 1.]
-    #     else:
-    #         pcov = pcov - [1., 1.]
-    #         pfc =  pfc -[1., 1., 1.]
-    #         f_name = compute_file_name(pcov, pfc)
-    #         pcov = pcov + [0.1, 0.1]
-    #         pfc =  pfc +[0.1, 0.1, 0.1]
-    #         working_level = level3
-    # if (working_level == level3):
-    #     if (acc >= 0.8):
-    #         f_name = compute_file_name(pcov, pfc)
-    #         pcov = pcov + [0.1, 0.1]
-    #         pfc =  pfc +[0.1, 0.1, 0.1]
-    #     else:
-    #         run = 0
-    #         print('finished')
-    #
-    #
-
     acc_list.append(acc)
     count = count + 1
     print (acc)
